8a091f07af5e16023d043ad306d8be6b5cb2cb24
amaity-digi <116334087+amaity-digi@users.noreply.github.com>
2023-09-02 02:04:23 +0530

    add two number using ts

---
+++ index.ts
@@ -0,0 +1,19 @@
+//TypeScript is nothing but JavaScript only with some extra power. TypeScript is the superset of JavaScript.
+
+console.log("TypeScript");
+
+ // TypeScript is all about static typing. this error we got ==> Type 'string' is not assignable to type 'number'
+
+ 
+//@ts-ignore 
+let n:number = 1000;
+n= 12;
+console.log(n);
+
+//Write a code to add two number using ts
+//@ts-ignore
+function sum(a:number,b:number):number{
+    return a+b;
+}
+
+console.log(sum(12,30));
